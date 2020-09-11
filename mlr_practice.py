61ef676bc8176739ab8b0d6a5c16c3449f0a7a14
alf808 <alf@akademe.net>
2020-09-10 16:55:10 -1000

    init commit

---
+++ mlr_practice.py
@@ -0,0 +1,282 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+
+from mpl_toolkits.mplot3d import Axes3D
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+#streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+url = 'https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv'
+res = requests.get(url)
+with open('manhattan.csv', 'wb') as file:
+    file.write(res.content)
+
+streeteasy = pd.read_csv('manhattan.csv')
+df = pd.DataFrame(streeteasy)
+
+x = df[['size_sqft','building_age_yrs']]
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+ols = LinearRegression()
+
+ols.fit(x_train, y_train)
+
+# Plot the figure
+
+fig = plt.figure(1, figsize=(6, 4))
+plt.clf()
+
+elev = 43.5
+azim = -110
+
+ax = Axes3D(fig, elev=elev, azim=azim)
+
+ax.scatter(x_train[['size_sqft']], x_train[['building_age_yrs']], y_train, c='k', marker='+')
+
+ax.plot_surface(np.array([[0, 0], [4500, 4500]]), np.array([[0, 140], [0, 140]]), ols.predict(np.array([[0, 0, 4500, 4500], [0, 140, 0, 140]]).T).reshape((2, 2)), alpha=.7)
+
+ax.set_xlabel('Size (ft$^2$)')
+ax.set_ylabel('Building Age (Years)')
+ax.set_zlabel('Rent ($)')
+
+ax.w_xaxis.set_ticklabels([])
+ax.w_yaxis.set_ticklabels([])
+ax.w_zaxis.set_ticklabels([])
+
+# Add the code below:
+plt.show()
+
+######################################222222222
+
+import codecademylib3_seaborn
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/Codecademy/datasets/master/streeteasy/queens.csv")
+
+print(df.head())
+
+########################################33333333333
+
+import codecademylib3_seaborn
+import pandas as pd
+
+# import train_test_split
+from sklearn.model_selection import train_test_split
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+df_col = df.columns.to_list()
+
+df_incl = ['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']
+
+x = df[df_incl]
+y = df['rent']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=6)
+
+print(x_train.shape, x_test.shape)
+
+########################4444444444444444444
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+# Add the code here:
+from sklearn.linear_model import LinearRegression
+
+mlr = LinearRegression()
+
+mlr.fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+
+#print(df[df.rental_id == 2177438]['rent'])
+# Sonny doesn't have an elevator so the 11th item in the list is a 0
+sonny_apartment = [[1, 1, 620, 16, 1, 98, 1, 0, 1, 0, 0, 1, 1, 0]]
+
+predict = mlr.predict(sonny_apartment)
+
+print("Predicted rent: $%.2f" % predict)
+
+
+
+###############################################555555555555555555555
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+lm = LinearRegression()
+
+model=lm.fit(x_train, y_train)
+
+y_predict = lm.predict(x_test)
+
+plt.scatter(y_test, y_predict)
+plt.xlabel('Actual Prices')
+plt.ylabel('Predicted Prices')
+plt.show()
+
+########################################6666666666666666666666666
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+mlr = LinearRegression()
+
+model=mlr.fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+
+# Input code here:
+print(model.coef_)
+print(mlr.coef_)
+
+##########################################777777777777777777777
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+# Input code here:
+plt.scatter(df['size_sqft'], df['rent'], alpha=.4)
+plt.scatter(df['min_to_subway'], df['rent'], alpha=.4)
+
+
+plt.show()
+plt.scatter(df['building_age_yrs'], df['rent'])
+plt.show()
+
+###############################################888888888888888888888888
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+mlr = LinearRegression()
+
+model=mlr.fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+
+# Input code here:
+print(model.score(x_train, y_train))
+print(model.score(x_test, y_test))
+
+
+
+###########################################999999999999999999999999
+
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+lm = LinearRegression()
+
+model = lm.fit(x_train, y_train)
+
+y_predict= lm.predict(x_test)
+
+print("Train score:")
+print(lm.score(x_train, y_train))
+
+print("Test score:")
+print(lm.score(x_test, y_test))
+
+plt.scatter(y_test, y_predict)
+plt.plot(range(20000), range(20000))
+
+plt.xlabel("Prices: $Y_i$")
+plt.ylabel("Predicted prices: $\hat{Y}_i$")
+plt.title("Actual Rent vs Predicted Rent")
+
+plt.show()
+print(model.coef_)
+# zoe_apartment = [[1, 1, 620, 16, 1, 98, 0, 0, 1, 0, 0, 0, 1, 0]]
+# predict = model.predict(zoe_apartment)
+# print("Predicted rent: $%.2f" % predict)
